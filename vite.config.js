803291ca5a956dbd854f867f25d47cb4556eb934
Ahmed Hamdy <ahamdi.hussein@gmail.com>
2026-01-08 00:04:15 +0400

    Fix Railway allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    allowedHosts: ['love-roro-game.up.railway.app', '.railway.app', 'all']
   },
   preview: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: ['love-roro-game.up.railway.app', '.railway.app', 'all']
   }
 })
